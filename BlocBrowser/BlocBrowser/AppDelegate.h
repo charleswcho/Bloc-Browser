890117fb0f7b3589d8ed5d0fc6a1d08105d944dd
Charles Wesley Cho <charleswcho@gmail.com>
2015-07-03 12:53:32 -0700

    Checkpoint 23 completed, Building BlocBrowser

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Charles Wesley Cho on 7/3/15.
+//  Copyright (c) 2015 Charles Wesley Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/BlocBrowser/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Charles Wesley Cho on 7/3/15.
+//  Copyright (c) 2015 Charles Wesley Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
